3b0c4ce0039715ccb199a447efbe084e6deeae13
Quan <hilda_quan@163.com>
2021-11-08 19:35:35 +0800

    2021-11-08：第四天。数据处理，数据显示，JSON

--- SpringMVC-06-JSON/src/main/java/com/quan/controller/UserController.java
+++ SpringMVC-06-JSON/src/main/java/com/quan/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.quan.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quan.pojo.User;
@@ -55,6 +57,45 @@ public class UserController {
         // 将对象解析成为 JSON 字符串
         String str = mapper.writeValueAsString(list);
         return str;
+    }
+
+
+    @RequestMapping("/j4")
+    public String json04() {
+        User user1 = new User("权1", 1, "man");
+        User user2 = new User("权2", 2, "man");
+        User user3 = new User("权3", 3, "man");
+        User user4 = new User("权4", 4, "man");
+        User user5 = new User("权5", 5, "man");
+
+        List<User> list = new ArrayList<User>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+
+        System.out.println("*******Java对象 转 JSON字符串*******");
+        String str1 = JSON.toJSONString(list);
+        System.out.println("JSON.toJSONString(list)==>"+str1);
+
+        String str2 = JSON.toJSONString(user1);
+        System.out.println("JSON.toJSONString(user1)==>"+str2);
+
+        System.out.println("\n****** JSON字符串 转 Java对象*******");
+        User jp_user1 = JSON.parseObject(str2,User.class);
+        System.out.println("JSON.parseObject(str2,User.class)==>"+jp_user1);
+
+        System.out.println("\n****** Java对象 转 JSON对象 ******");
+        JSONObject jsonObject1 = (JSONObject) JSON.toJSON(user2);
+        System.out.println("(JSONObject) JSON.toJSON(user2)==>"+jsonObject1.getString("name"));
+
+        System.out.println("\n****** JSON对象 转 Java对象 ******");
+        User to_java_user = JSON.toJavaObject(jsonObject1, User.class);
+        System.out.println("JSON.toJavaObject(jsonObject1, User.class)==>"+to_java_user);
+
+
+        return "hello";
     }
 
 }
